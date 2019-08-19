15124e9cc3d83f003676433f072a1a107b6d09bc
Robin Gloster <mail@glob.in>
2019-08-19 16:28:28 +0200

    stdenv.mkDerivation: fix further structuredAttrs issues

--- pkgs/development/compilers/gcc/7/default.nix
+++ pkgs/development/compilers/gcc/7/default.nix
@@ -238,11 +238,11 @@ stdenv.mkDerivation ({
   # compiler (after the specs for the cross-gcc are created). Having
   # LIBRARY_PATH= makes gcc read the specs from ., and the build breaks.
 
-  CPATH = optionals (targetPlatform == hostPlatform) (makeSearchPathOutput "dev" "include" ([]
+  CPATH = toString (optionals (targetPlatform == hostPlatform) (makeSearchPathOutput "dev" "include" ([]
     ++ optional (zlib != null) zlib
-  ));
+  )));
 
-  LIBRARY_PATH = optionals (targetPlatform == hostPlatform) (makeLibraryPath (optional (zlib != null) zlib));
+  LIBRARY_PATH = toString (optionals (targetPlatform == hostPlatform) (makeLibraryPath (optional (zlib != null) zlib)));
 
   inherit
     (import ../common/extra-target-flags.nix {
